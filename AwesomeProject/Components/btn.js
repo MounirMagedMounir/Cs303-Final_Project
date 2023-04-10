535b858580df944f0f51f7da5aba4dcd116cd2c3
aliYasser <ay6516169@gmail.com>
2023-04-10 16:27:26 +0200

    update register && component

--- AwesomeProject/Components/btn.js
+++ AwesomeProject/Components/btn.js
@@ -1,20 +1,21 @@
-import { Text, TouchableOpacity } from "react-native";
-import React from "react";
+import {Text, TouchableOpacity} from 'react-native';
+import React from 'react';
 
-export default function Botton({ label, onPress }) {
+export default function Botton({label, onPress}) {
   return (
     <TouchableOpacity
       onPress={onPress}
       style={{
-        width: "100%",
-        backgroundColor: "#FE3539",
-        padding: 10,
-        borderRadius: 50,
-        marginTop: 0,
-        marginLeft: 0,
-      }}
-    >
-      <Text style={{ color: "white", textAlign: "center" }}>{label}</Text>
+        width: '100%',
+    backgroundColor: '#539165',
+    padding: 10,
+    borderRadius: 50,
+    marginTop: 0,
+    marginLeft: 0,
+      }}>
+      <Text style={{   color:"white",textAlign:"center"}}>
+        {label}
+      </Text>
     </TouchableOpacity>
   );
 }
