155d325c4df69f01b1546ff9a1fa7eb3ba808470
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2023-04-18 12:23:05 +0200

    creating product

---
+++ AwesomeProject/Screens/Product.js
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  Animated,
+  useWindowDimensions,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+const Product = () => {
+  return (
+    <Text>Product</Text>
+  )
+}
+
+export default Product
