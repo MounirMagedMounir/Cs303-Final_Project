0949c3c9e6b716ec6c013207ed514ec994d812e0
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2023-04-18 03:40:05 +0200

    update navigations

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,27 +1,9 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./Screens/Home";
-import Login from "./Screens/Login";
-import Signup from "./Screens/Register";
-import About from "./Screens/About";
-const Stack = createNativeStackNavigator();
+import NavBar from "./Components/NavBar";
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen
-          name="Register"
-          component={Signup}
-          options={{ title: "Signup" }}
-        />
-        <Stack.Screen name="About" component={About} />
-        {/* <Stack.Screen name="About" component={About} />
-      <Stack.Screen name="About" component={About} />
-      <Stack.Screen name="About" component={About} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+ 
+   <NavBar />
+ 
   );
 }
