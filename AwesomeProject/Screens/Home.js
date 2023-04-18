503a12faa28d749f8e9be03f31ef7a81b1992f7b
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2023-04-18 12:15:06 +0200

    updata home

--- AwesomeProject/Screens/Home.js
+++ AwesomeProject/Screens/Home.js
@@ -12,10 +12,10 @@ import {
   ImageBackground,
   Image,
 } from "react-native";
-import { useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth ,db } from "../firebase";
+import { auth, db } from "../firebase";
 import SlideShow from "../Components/SlideShow";
 
 const Imgurl = [
@@ -23,8 +23,11 @@ const Imgurl = [
   "https://images.pexels.com/photos/906150/pexels-photo-906150.jpeg?auto=compress&cs=tinysrgb&w=400",
   "https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=400",
   "https://images.pexels.com/photos/4482677/pexels-photo-4482677.jpeg?auto=compress&cs=tinysrgb&w=400",
-  "https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=400",
-  "https://images.pexels.com/photos/4482677/pexels-photo-4482677.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/133472/pexels-photo-133472.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/39574/flower-exotic-colorful-pink-39574.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/668465/pexels-photo-668465.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/697259/pexels-photo-697259.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/639086/pexels-photo-639086.jpeg?auto=compress&cs=tinysrgb&w=1600",
 ];
 
 export default function Home({ navigation }) {
@@ -34,165 +37,141 @@ export default function Home({ navigation }) {
 
   const [State, setState] = useState(false);
 
-  const [UserData,SetUserData]=useState(null);
+  const [UserData, SetUserData] = useState(null);
 
-  const[authUser,setAuthUser]=useState(null);
+  const [authUser, setAuthUser] = useState(null);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     getUserData();
-    const listen=onAuthStateChanged(auth,(user)=>{
-          if(user){
-              setAuthUser(user);  
-              setState(true);
-          }else{
-              setAuthUser(null);  
-              setState(false);
-          }
-      });
-  return()=>{
-      listen();
-    }
-  },[]);
-  
-
-useEffect(()=>{
-  const listen=onAuthStateChanged(auth,(user)=>{
-      if(user){
-          setAuthUser(user);  
-          setState(true);
-      }else{
-          setAuthUser(null);  
-          setState(false);
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+        setState(true);
+      } else {
+        setAuthUser(null);
+        setState(false);
       }
-  });
-return()=>{
-  listen();
-}
-},[]);
+    });
+    return () => {
+      listen();
+    };
+  }, []);
 
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+        setState(true);
+      } else {
+        setAuthUser(null);
+        setState(false);
+      }
+    });
+    return () => {
+      listen();
+    };
+  }, []);
 
-
-  const getUserData=async()=>{
-    const q = query(collection(db, "users"), where("uid", "==", auth.currentUser.uid));
+  const getUserData = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("uid", "==", auth.currentUser.uid)
+    );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       console.log(doc.id, " => ", doc.data());
       SetUserData(doc.data());
-    });
-  };
-
-  const logOut = () => {
-    signOut(auth)
-      .then(() => {})
-      .catch((error) => {});
+    });
   };
 
-  // onAuthStateChanged(auth, (user) => {
-  //   if (user) {
-  //     setName(user.email);
-  //     setState(true);
-  //     const uid = user.uid;
-  //   } else {
-  //     setState(false);
-  //   }
-  // });
-console.log(UserData);
+  console.log(UserData);
 
   return (
-
     <View style={styles.container}>
-    <ScrollView>
-    <View style={styles.container}>
-        {State == true ? (
-          <View>
-            <TouchableOpacity onPress={() => logOut()}>
-              <Image
-                source={{
-                  uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6CFGuwOszeow3Iq3_cRnwWoIc83-GUB0fVQ&usqp=CAU",
-                }}
-                style={styles.logOutL}
-              />
-            </TouchableOpacity>
-            <Text style={{fontSize:24,fontWeight:'bold',marginBottom:20}}>Hello {UserData?.name}</Text>
-          </View>
-        ) : (
-          <View>
-           
-          </View>
-        )}
-      </View>
-      <SafeAreaView>
-        <View style={styles.scrollContainer}>
-          <ScrollView
-            horizontal={true}
-            pagingEnabled
-            showsHorizontalScrollIndicator={false}
-            onScroll={Animated.event([
-              {
-                nativeEvent: {
-                  contentOffset: {
-                    x: scrollX,
+      <ScrollView>
+        <View style={styles.container}>
+          {State == true ? (
+            <View>
+              <Text
+                style={{ fontSize: 24, fontWeight: "bold", marginBottom: 20 }}
+              >
+                Hello {UserData?.name}
+              </Text>
+            </View>
+          ) : (
+            <View></View>
+          )}
+        </View>
+        <SafeAreaView>
+          <View style={styles.scrollContainer}>
+            <ScrollView
+              horizontal={true}
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              onScroll={Animated.event([
+                {
+                  nativeEvent: {
+                    contentOffset: {
+                      x: scrollX,
+                    },
                   },
                 },
-              },
-            ])}
-            scrollEventThrottle={1}
-          >
-            {Imgurl.map((image, imageIndex) => {
-              return (
-                <View>
-                  <TouchableOpacity
-                    key={imageIndex}
-                    onPress={() => navigation.navigate("Login")}
-                  >
-                    <View
-                      style={{ width: windowWidth, height: 250 }}
+              ])}
+              scrollEventThrottle={1}
+            >
+              {Imgurl.map((image, imageIndex) => {
+                return (
+                  <View>
+                    <TouchableOpacity
                       key={imageIndex}
+                      onPress={() => navigation.navigate("Login")}
                     >
-                      <ImageBackground
-                        source={{ uri: image }}
-                        style={styles.HeaderCard}
-                      ></ImageBackground>
-                    </View>
-                  </TouchableOpacity>
-                </View>
-              );
-            })}
-          </ScrollView>
-          <View style={styles.indicatorContainer}>
-            {Imgurl.map((image, imageIndex) => {
-              const width = scrollX.interpolate({
-                inputRange: [
-                  windowWidth * (imageIndex - 1),
-                  windowWidth * imageIndex,
-                  windowWidth * (imageIndex + 1),
-                ],
-                outputRange: [8, 16, 8],
-                extrapolate: "clamp",
-              });
-              return (
-                <Animated.View
-                  key={imageIndex}
-                  style={[styles.normalDot, { width }]}
-                />
-              );
-            })}
+                      <View
+                        style={{ width: windowWidth, height: 250 }}
+                        key={imageIndex}
+                      >
+                        <ImageBackground
+                          source={{ uri: image }}
+                          style={styles.HeaderCard}
+                        ></ImageBackground>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                );
+              })}
+            </ScrollView>
+            <View style={styles.indicatorContainer}>
+              {Imgurl.map((image, imageIndex) => {
+                const width = scrollX.interpolate({
+                  inputRange: [
+                    windowWidth * (imageIndex - 1),
+                    windowWidth * imageIndex,
+                    windowWidth * (imageIndex + 1),
+                  ],
+                  outputRange: [8, 16, 8],
+                  extrapolate: "clamp",
+                });
+                return (
+                  <Animated.View
+                    key={imageIndex}
+                    style={[styles.normalDot, { width }]}
+                  />
+                );
+              })}
+            </View>
           </View>
+        </SafeAreaView>
+
+        <View>
+          <SlideShow Imgurl={Imgurl} CatName="men" />
+          <SlideShow Imgurl={Imgurl} CatName="women" />
+          <SlideShow Imgurl={Imgurl} CatName="east" />
+          <SlideShow Imgurl={Imgurl} CatName="west" />
         </View>
-      </SafeAreaView>
-
-      <View>
-        <SlideShow Imgurl={Imgurl} CatName="men" />
-        <SlideShow Imgurl={Imgurl} CatName="women" />
-        <SlideShow Imgurl={Imgurl} CatName="east" />
-        <SlideShow Imgurl={Imgurl} CatName="nourth" />
-      </View>
-
-    </ScrollView>
-  </View>
-);
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -212,7 +191,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   scrollContainer1: {
-    height: 250,        width: 400,
+    height: 250,
+    width: 400,
     marginBottom: 10,
     alignItems: "center",
     justifyContent: "center",
