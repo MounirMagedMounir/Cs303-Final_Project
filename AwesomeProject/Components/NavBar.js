399bf9b780a805ae3b5a1dae3b5c3aa6f1c07e81
alinasser2 <44727847+alinasser2@users.noreply.github.com>
2023-04-18 03:38:01 +0200

    update navbar

--- AwesomeProject/Components/NavBar.js
+++ AwesomeProject/Components/NavBar.js
@@ -3,6 +3,16 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { auth } from "../firebase";
+import { useState, useEffect } from "react";
+import {
+  View,
+} from "react-native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const stack = createNativeStackNavigator();
+
+
 
 //screens import 
 import Home from '../Screens/Home';
@@ -10,6 +20,9 @@ import Login from '../Screens/Login';
 import Register from '../Screens/Register';
 import About from '../Screens/About';
 import More from '../Screens/More';
+import Cart from '../Screens/Cart';
+import Profile from '../Screens/Profile';
+import Product from '../Screens/Product';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,11 +31,26 @@ const loginName = "Login";
 const registerName = "Register";
 const aboutName = "About";
 const moreName = "More";
+const cartName = "Cart";
+const profileName = "Profile";
+const productName = "Products";
 
-const auth = getAuth();
 
 function NavBar() 
 {
+  const [State, setState] = useState(false);
+
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setState(true);
+      const uid = user.uid;
+    } else {
+      setState(false);
+    }
+  });
+
+
     return (
 
       
@@ -45,32 +73,59 @@ function NavBar()
                 else if (rn === registerName) {
                     iconName = focused ? 'person-add' : 'person-add-outline';
                 }
-                else if (rn === aboutName) {
-                    iconName = focused ? 'information-circle' : 'information-circle-outline';
-                }
+                // else if (rn === aboutName) {
+                //     iconName = focused ? 'information-circle' : 'information-circle-outline';
+                // }
                 else if (rn === moreName) {
                     iconName = focused ? 'ellipsis-horizontal' : 'ellipsis-horizontal-outline';
                 }
+                else if (rn === cartName) {
+                    iconName = focused ? 'cart' : 'cart-outline';
+                }
+                else if (rn === profileName) {
+                    iconName = focused ? 'person' : 'person-outline';
+                }
+
+                else if (rn === productName) {
+                  // add all products icons hereshopping-bag
+
+                  iconName = focused ? 'grid' : 'grid-outline';
+                }
                 
     
-                // You can return any component that you like here!
+                // return components
                 return <Ionicons name={iconName} size={size} color={color} />;
               },
             })}
             tabBarOptions={{
-                activeTintColor: 'tomato',
+                activeTintColor: '#539165',
                 inactiveTintColor: 'grey',
                 labelStyle: { paddingBottom: 10, fontSize: 10 },
                 style: { padding: 10, height: 70}
               }}
               >
 
-            <Tab.Screen name={homeName} component={Home} />
-            <Tab.Screen name={loginName} component={Login} />
-            <Tab.Screen name={registerName} component={Register} />
-            <Tab.Screen name={aboutName} component={About} />
+                <Tab.Screen name={homeName} component={Home} />
+                <Tab.Screen name={productName} component={Product} />
+
+                {State == true ? (
+                <Tab.Screen name={profileName} component={Profile} />
+                  ) : (
+
+                <Tab.Screen name={loginName} component={Login} />
+                )}
+
+                {State == true ? (
+                <Tab.Screen name={cartName} component={Cart} />
+                  ) : (
+                <Tab.Screen name={registerName} component={Register}/>
+                )}
+
             <Tab.Screen name={moreName} component={More} />
+            
             </Tab.Navigator>
+
+            
         </NavigationContainer>
         
 
