5af0ea0c3e7bcfca4b1aecb60393cfd84286a899
aliYasser <ay6516169@gmail.com>
2023-05-02 13:34:02 +0200

    CartContext

---
+++ AwesomeProject/CartContext.js
@@ -0,0 +1,74 @@
+import React, {createContext, useState} from "react";
+// import {getProduct} from "./services/ProductsService.js";
+import {getProduct} from "./services/product.js"
+export const CartContext = createContext();
+
+export function CartProvider(props){
+
+    const [items, setItems] = useState([]);
+
+    function addItemToCart(id){
+        const product = getProduct(id);
+        setItems((prevItems) => {
+            const item = prevItems.find((item) => (item.id == id));
+            if(!item){
+                return [...prevItems, {
+                    id, 
+                    qty: 1,
+                    product, 
+                    totalPrice: product.price
+                }]
+            }else{
+                return prevItems.map((item) => {
+                    if(item.id == id){
+                        item.qty++;
+                        item.totalPrice += product.price;
+                    }
+                    
+                    return item;
+                })
+            }
+        })
+    }
+    function deleteItemToCart(id){
+        const product = getProduct(id);
+        setItems((prevItems) => {
+            const item = prevItems.find((item) => (item.id == id));
+            if(!item){
+                return [...prevItems, {
+                    id, 
+                    qty: 1,
+                    product, 
+                    totalPrice: product.price
+                }]
+            }else{
+                return prevItems.map((item) => {
+                    if(item.id == id){
+                     if(item.qty>0){
+                         item.qty--;
+                        item.totalPrice -= product.price;
+                     }else{
+                        item.qty=0;
+                     }
+                       
+                    }
+                    return item;
+                })
+            }
+        })
+    }
+    function getItemsCount(){
+        return items.reduce((sum,item) => (sum + item.qty), 0)
+    }
+
+    function getTotalPrice(){
+        return items.reduce((sum, item) => (sum + item.totalPrice), 0)
+    }
+
+    return(
+        <CartContext.Provider value={{items,deleteItemToCart,getItemsCount, addItemToCart, getTotalPrice}}>
+            {props.children}
+        </CartContext.Provider>
+    )
+
+}
