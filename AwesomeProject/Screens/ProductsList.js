c1058efcccc2229be4dce8ab2cbcd7d8242d4d27
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2023-05-09 04:58:03 +0300

    Create Filter

--- AwesomeProject/Screens/ProductsList.js
+++ AwesomeProject/Screens/ProductsList.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { auth, db } from '../firebase'
-
+import { auth, db } from "../firebase";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import {
   View,
   Text,
   FlatList,
   StyleSheet,
   ScrollView,
-  Pressable,
-  Image,
+  Pressable,Button,
+  Image,TouchableOpacity
 } from "react-native";
 // import { getProducts } from "../services/ProductsService";
 // import { Product } from "../Components/product";
@@ -17,68 +17,138 @@ import { Box, Center, Flex, Heading, VStack } from "native-base";
 import Rating from "../Components/Rating";
 // import { LinearGradient } from "expo-linear-gradient";
 // import { TouchableOpacity } from "react-native-gesture-handler";
-import { collection, query, where, getDocs, updateDoc, doc, onSnapshot, orderBy } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
+import { TextInput } from "react-native-paper";
 
 export default function ProductsList({ navigation }) {
-  // function renderProduct({item: product}){
-  // return(
-  //     <Product
-  //         {...product}
-  //         onPress={() => {
-  //             navigation.navigate('ProductDetails', {productId: product.id})
-  //         }}
-  //     />
-  // )
-  // return <Box flex="1" safeAreaTop>
-  // <ScrollView>
-  //   <VStack space={2.5} w="100%" px="3">
-  // <Flex direction="row" mb="2.5" mt="1.5">
-  // <Center size="16" bg="primary.100" _text={{
-  // color: "coolGray.800"
-  // }}>
-  //   {product.name}
-  // </Center>
-  // <Center size="16" bg="primary.100" _text={{
-  // color: "coolGray.800"
-  // }}>
-  //   {product.name}
-  // </Center>
 
-  // </Flex>
-  //         </VStack>
-  //       </ScrollView>
-  //     </Box>;
-
-  // }
   const [data, SetData] = useState([]);
-  const [products, setProducts] = useState([]);
-  useLayoutEffect(()=>{
-    const ref=collection(db,"Products");
-    onSnapshot(ref,(Products)=>
-    SetData(Products.docs.map((Product)=>({
-        id:Product.uid,
-        data:Product.data()
-})))
-    )
-})
+  const [filterStat, setfilterStat] = useState(false);
+  const [filteron, setfilteron] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+ 
+  const handleSearch = async (searchText) => {
+    try {
+      const q = query(collection(db, 'Products'), where('category', '==', searchText));
+      const querySnapshot = await getDocs(q);
 
+      const results = querySnapshot.docs.map((Product) => ({
+        id: Product.uid,
+        data: Product.data(),
+      }));
+      setfilterStat(true);
+      setSearchResults(results);
+      console.log(results);
+      console.log(data);
+    } catch (error) {
+      console.error('Error searching products:', error);
+    }
+  };
+
+
+  useLayoutEffect(() => {
+    
+    const ref = collection(db, "Products");
+    onSnapshot(ref, (Products) =>
+      SetData(
+        Products.docs.map((Product) => ({
+          id: Product.uid,
+          data: Product.data(),
+        }))
+      )
+    );
+  });
+
+
+ 
   return (
-    // <ScrollView flex={1} marginTop={10} showsVerticalScrollIndicator={false}>
-
-    //     <Flex flexWrap="wrap" direction="row" justifyContent="space-between" px={6}>
-
-    // {products.map((product )=>(
-
-    //   <Pressable key={product.id} style={{backgroundColor:"#fff",width:100,shadow:2,paddingTop:0.3,paddingBottom:2,rounded:"md",overflow:"hidden",marginBottom:10,marginHorizontal:27}}  onPress={() => {navigation.navigate('ProductDetails', {productId: product.id})}}>
-    // <Image source={product.image} alt={product.name}  style={{height:70,width:100}} resizeMode="contain" />
-    //   <Box px={4} pt={1}>
-    //     <Heading size="sm" bold ml={3}>${product.price}</Heading>
-    //   <Text style={{fontSize:10,marginTop:1,marginLeft:2}}  w="full">{product.name}</Text>
-    //    <Rating value={product.rating}/>
-    //     </Box>
-
-    //    </Pressable>
+   
     <ScrollView flex={1} marginTop={10} showsVerticalScrollIndicator={false}>
+       {/* <TextInput
+          style={{ height: 40 }}
+          placeholder="Filter"
+          onChangeText={(newText) => setSearchText(newText)}
+          defaultValue={searchText}
+        /> */}
+{ filteron==true?
+<TouchableOpacity  
+   onPress={() => setfilteron(false)}>
+        <Ionicons  style={{
+              height: 40,
+          
+              marginLeft: 20,
+              marginTop: 2,
+              marginBottom: 10,
+              width: 60,
+              
+            }}
+            size={50}
+            name="close-outline"></Ionicons>
+            </TouchableOpacity>:
+<TouchableOpacity onPress={() => setfilteron(true)}>
+        <Ionicons     
+        style={{
+              height: 42,
+          
+              marginLeft: 20,
+              marginTop: 2,
+              marginBottom: 10,
+              width: 60,
+              
+            }}
+            size={47}
+            name="options-outline"></Ionicons>
+            </TouchableOpacity>
+
+}
+             {filteron==true? <>
+            
+
+
+<View style={{  flexDirection: "row",}}>
+{data.map((item, key) => (
+          
+          <TouchableOpacity style={{ 
+            backgroundColor: "#FFFFFF",
+          borderRadius: 50,
+          borderWidth: 1,
+          borderColor: "#000000",
+          paddingVertical: 10,
+          paddingHorizontal: 20,
+          marginVertical: 10,}}
+          onPress={() => handleSearch(item.data.category)}
+          >
+         <Text>{item.data.category}</Text>  
+          
+           </TouchableOpacity>
+        ))}
+       </View>
+       </>
+       :null}
+      
+        {/* <Button
+
+        title="X"
+        onPress={() => setfilterStat(false)}
+      /> */}
+
+        {/* <TouchableOpacity>
+        <Text style={{ fontSize: 42 }}>
+          {filter
+            .split(" ")
+            .map((word) => word)
+            .join(" ")}
+        </Text>
+        </TouchableOpacity> */}
       <Flex
         flexWrap="wrap"
         direction="row"
@@ -87,26 +157,38 @@ export default function ProductsList({ navigation }) {
         marginLeft={-3}
         marginRight={3}
       >
-       {data.map((item,key)=>(
+       {filterStat==true? 
+       <>
+       {searchResults.map((item, key) => (
           <Pressable
+          style={{
+            height: 250,
+            elevation: 2,
+            backgroundColor: "#FFF",
+            marginLeft: 20,
+            marginTop: 15,
+            borderRadius: 15,
+            marginBottom: 10,
+            width: 160,
+          }}
             key={key}
             onPress={() => {
-              navigation.navigate("ProductDetails", { productId: item.data.uid });
+              navigation.navigate("ProductDetails", {
+                productId: item.data.uid,
+              });
             }}
-            style={{
-              height: 250,
-              elevation: 2,
-              backgroundColor: "#FFF",
-              marginLeft: 20,
-              marginTop: 15,
-              borderRadius: 15,
-              marginBottom: 10,
-              width: 160,
-            }}
+        
           >
-         
-       
-            <Image  source={{uri:item.data.IMG}}  style={{width:"100%",height:"70%",resizeMode:"cover",borderRadius:5,marginTop:0}}/>
+            <Image
+              source={{ uri: item.data.IMG }}
+              style={{
+                width: "100%",
+                height: "70%",
+                resizeMode: "cover",
+                borderRadius: 5,
+                marginTop: 0,
+              }}
+            />
             <View
               style={{
                 flexDirection: "row",
@@ -131,11 +213,70 @@ export default function ProductsList({ navigation }) {
                 {item.data.price}$
               </Text>
             </View>
-            <View style={{ marginLeft: 10}}>
+            <View style={{ marginLeft: 10 }}>
               <Rating value={item.data.rating} />
             </View>
           </Pressable>
         ))}
+        </>:<>
+        {data.map((item, key) => (
+          <Pressable
+            key={key}
+            onPress={() => {
+              navigation.navigate("ProductDetails", {
+                productId: item.data.uid,
+              });
+            }}
+            style={{
+              height: 250,
+              elevation: 2,
+              backgroundColor: "#FFF",
+              marginLeft: 20,
+              marginTop: 15,
+              borderRadius: 15,
+              marginBottom: 10,
+              width: 160,
+            }}
+          >
+            <Image
+              source={{ uri: item.data.IMG }}
+              style={{
+                width: "100%",
+                height: "70%",
+                resizeMode: "cover",
+                borderRadius: 5,
+                marginTop: 0,
+              }}
+            />
+            <View
+              style={{
+                flexDirection: "row",
+                paddingTop: 15,
+                paddingHorizontal: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                {item.data.name}
+              </Text>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  color: "#00a46c",
+                  paddingLeft: 35,
+                }}
+              >
+                {item.data.price}$
+              </Text>
+            </View>
+            <View style={{ marginLeft: 10 }}>
+              <Rating value={item.data.rating} />
+            </View>
+          </Pressable>
+        ))}</>}
       </Flex>
     </ScrollView>
   );
